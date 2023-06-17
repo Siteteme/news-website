ab88afd22faf51e44fdb0ebc2f5c444097311207
farid <siteteme@gmail.com>
2023-06-17 04:57:39 +0430

    slider fixed

---
+++ src/components/HomePageComponents/Intro/ItemSlider.js
@@ -0,0 +1,62 @@
+import banner from "../../../assets/img/banner.png";
+import arrowSlider from "../../../assets/img/arrow-slider.png";
+const ItemSlider = () => {
+  return (
+    <>
+      <div>
+        <div className="w-[88px] before:transition-all h-fit relative before:bg-black before:overflow-hidden overflow-hidden before:absolute before:w-full before:h-full before:rounded-[10px] before:bg-opacity-60">
+          <img
+            src={banner}
+            alt="banner"
+            className="h-[220px] object-cover  rounded-[10px]  "
+          />
+        </div>
+        <div className="relative bottom-12 flex flex-col items-center">
+          <img
+            src={arrowSlider}
+            alt="arrow-slider"
+            className="w-full max-w-fit"
+          />
+          <hr className="w-[85%] bg-white h-[1px] mt-2 mx-2" />
+        </div>
+      </div>
+      <div>
+        <div className="w-[88px] before:transition-all h-fit relative before:bg-black before:overflow-hidden overflow-hidden before:absolute before:w-full before:h-full before:rounded-[10px] before:bg-opacity-60">
+          <img
+            src={banner}
+            alt="banner"
+            className="h-[220px] object-cover  rounded-[10px]  "
+          />
+        </div>
+        <div className="relative bottom-12 flex flex-col items-center">
+          <img
+            src={arrowSlider}
+            alt="arrow-slider"
+            className="w-full max-w-fit"
+          />
+          <hr className="w-[85%] bg-white h-[1px] mt-2 mx-2" />
+        </div>
+      </div>
+
+      <div className="w-[450px] relative  ">
+        <img
+          src={banner}
+          alt="banner"
+          className="h-[220px] object-cover rounded-[10px] w-full "
+        />
+        <div className="flex justify-between items-center w-auto m-auto bg-[rgba(0,0,0,0.5)] p-2 rounded  backdrop-blur-[6px] relative bottom-14 mx-4 border text-sm cursor-pointer">
+          <span className="text-white">
+            زیــبا ترین مناطق تاریخی ایــران را بــشناســید
+          </span>
+          <img
+            src={arrowSlider}
+            alt="arrow-slider"
+            className="w-full max-w-fit"
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ItemSlider;
--- src/components/HomePageComponents/Intro/Slider.js
+++ src/components/HomePageComponents/Intro/Slider.js
@@ -1,35 +1,9 @@
-import banner from "../../../assets/img/banner.png";
-import arrowSlider from "../../../assets/img/arrow-slider.png";
+import ItemSlider from "./ItemSlider";
+
 const Slider = () => {
   return (
     <section className="flex gap-2  ">
-      <div className="w-[88px] ">
-        <img
-          src={banner}
-          alt="banner"
-          className="h-[220px] object-cover rounded-[10px] relative "
-        />
-      </div>
-      <div className="w-[88px] ">
-        <img
-          src={banner}
-          alt="banner"
-          className="h-[220px] object-cover  rounded-[10px] opacity-10    "
-        />
-      </div>
-      <div className="w-[450px] relative  ">
-        <img
-          src={banner}
-          alt="banner"
-          className="h-[220px] object-cover rounded-[10px] w-full "
-        />
-        <div className="flex justify-between items-center w-auto m-auto bg-[rgba(0,0,0,0.5)] p-2 rounded  backdrop-blur-[6px] relative bottom-14 mx-4 border text-sm cursor-pointer">
-          <span className="text-white">
-            زیــبا ترین مناطق تاریخی ایــران را بــشناســید
-          </span>
-          <img src={arrowSlider} alt="arrow-slider" />
-        </div>
-      </div>
+      <ItemSlider />
     </section>
   );
 };
